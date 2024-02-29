3bf09caf0428710f445b50f736138b185e5a853d
Katekaru Soma <m0a2008038@edu.teu.ac.jp>
2024-02-29 18:36:48 +0900

    [wip]キャラクタープレハブとキャクターコアのスクリプト修正。キャラクターインフォ、エネミータワー追加。バトルステージ追加。メニューのキャラクターパネル追加。

--- Assets/Script/Battle/CharacterCore.cs
+++ Assets/Script/Battle/CharacterCore.cs
@@ -98,8 +98,10 @@ namespace Battle
         public enum CharacterId
         {
             Volcus_01, Volcus_02, Era_01, Eleth_01, Orend_01, Sara_01, Shandy_01, Loxy_01,
-            Npc_01, Npc_02, Npc_03,
-            Enemy_04, Enemy05, Enemy_06, Enemy_16, Enemy_20
+            Npc_01, Npc_02, Npc_03, Npc_04, Npc_05, Npc_06, Npc_07, Npc_08, Npc_09, Npc_10, Npc_11, Npc_12, Npc_13, Npc_14,
+            Summon_01, Summon_02, Summon_03, Summon_04,
+            Enemy_01,Enemy_02,Enemy_03,Enemy_04,Enemy05,Enemy_06,Enemy_07,Enemy_08,Enemy_09,Enemy_10,Enemy_11,Enemy_12,Enemy_13,Enemy_14,Enemy_15,Enemy_16,Enemy_17,Enemy_18,Enemy_19,Enemy_20, Enemy_21, Enemy_22, Enemy_23,
+            Enemy_24, Enemy_25, Enemy_26, Enemy_27, Enemy_28, Enemy_29, Enemy_30, Enemy_31, Enemy_32, Enemy_33,
         }
 
         //キャラクターの種類=>味方or敵
