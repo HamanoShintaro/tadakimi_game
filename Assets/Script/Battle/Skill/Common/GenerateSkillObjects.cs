9193b5ae54b63f4317da4b871ae490a0acb4acd0
岡本竜弥 <>
2024-10-17 21:36:52 +0900

    [fix] ロクシーのスキル修正

--- Assets/Script/Battle/Skill/Common/GenerateSkillObjects.cs
+++ Assets/Script/Battle/Skill/Common/GenerateSkillObjects.cs
@@ -13,17 +13,18 @@ public class GenerateSkillObjects : MonoBehaviour
     [SerializeField]
     private Vector2 generateOffset;
 
-    private void OnEnable()
+    public void GenerateSkillObject()
     {
-        StartCoroutine(GenerateSkillObjectRoutine());
+        StartCoroutine(GenerateSkillObjectCoroutine());
     }
 
-    private IEnumerator GenerateSkillObjectRoutine()
+    private IEnumerator GenerateSkillObjectCoroutine()
     {
         yield return new WaitForSeconds(generateInterval);
         Vector3 generatePosition = new Vector3(transform.position.x + generateOffset.x, transform.position.y + generateOffset.y, transform.position.z);
         var skillObject = Instantiate(skillPrefab, generatePosition, transform.rotation);
         skillObject.transform.parent = GameObject.Find("Canvas_Static/[BackPanel]").transform;
+        Debug.Log("GenerateSkillObject");
     }
 
     private void OnDisable()
