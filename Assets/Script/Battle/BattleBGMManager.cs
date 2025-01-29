9c9628689d5b131b375452fa0b5e955133f274dd
ryuRyuGameMfg <ryuyaokamoto0717@gmail.com>
2025-01-30 00:15:08 +0900

    [add] BGMの追加

---
+++ Assets/Script/Battle/BattleBGMManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// バトルシーンのBGMを管理するクラス。
+/// ステージ番号の下二桁をリストのインデックスとして使用し、
+/// そのままAudioSourceを再生する実装例です。
+/// </summary>
+public class BattleBGMManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> audioClips;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        // BattleManagerから現在のステージ番号を取得する想定
+        int currentStage = int.Parse(PlayerPrefs.GetString(PlayerPrefabKeys.currentStageId));
+
+        // ステージ番号の下二桁をインデックスとして利用
+        // 例) 101なら 101 % 100 = 1, 102なら 2
+        int listIndex = currentStage % 100;
+
+        // リストの範囲内かチェック
+        if (listIndex >= 0 && listIndex < audioClips.Count)
+        {
+            // AudioClipが設定されているなら再生
+            if (audioClips[listIndex] != null)
+            {
+                audioSource.clip = audioClips[listIndex];
+                audioSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning($"AudioClipが設定されていません。ステージ番号: {currentStage}, リスト番号: {listIndex}");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"AudioClipリストの範囲外です。ステージ番号: {currentStage}, リスト番号: {listIndex}");
+        }
+    }
+}
